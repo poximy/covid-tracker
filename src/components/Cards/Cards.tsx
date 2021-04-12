6ff04e2a798d0ac61a215c212ff3d1c518fbff01
Erick ZV <zv.erick@protonmail.com>
2021-04-12 16:21:21 -0500

    Create initial component files

---
+++ src/components/Cards/Cards.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cards: React.FC = () => {
+  return <p>Cards</p>;
+};
+
+export default Cards;
---
+++ src/components/CountryPicker/CountryPicker.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CountryPicker: React.FC = () => {
+  return <p>CountryPicker</p>;
+};
+
+export default CountryPicker;
