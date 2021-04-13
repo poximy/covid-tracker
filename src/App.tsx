1fae3c74b27bc6185277bf8862f9d315f3fb7890
Erick ZV <zv.erick@protonmail.com>
2021-04-13 13:22:47 -0500

    Connect api to card

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { Cards, Chart, CountryPicker } from "./components";
+import fetchData from "./api";
+
+import styles from "./App.module.css";
+
+interface ResponseModel {
+  confirmed: number;
+  recovered: number;
+  deaths: number;
+  lastUpdate: string;
+}
 
 const App: React.FC = () => {
+  const [data, setData] = useState<ResponseModel>();
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetchData();
+      setData(response);
+    })();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Cards />
+      <Cards data={data} />
       <CountryPicker />
       <Chart />
     </div>
